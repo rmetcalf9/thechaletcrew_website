44d634d7a65fd84ded70dd3dab6271cfc5d93f6a
Robert <rmetcalf9@googlemail.com>
2024-02-12 19:52:08 +0000

    Deleting unneeded

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,12 +5,7 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/HomePage.vue') },
-      { path: 'about', component: () => import('pages/AboutPage.vue') },
-      { path: 'privacy', component: () => import('pages/PrivacyPolicy.vue') },
-      { path: 'faq', component: () => import('pages/Faq.vue') },
-      { path: 'contact', component: () => import('pages/ContactPage.vue') },
-      { path: 'postsubscribe', component: () => import('pages/Postsubscribe.vue') },
-      { path: 'calc/brr', component: () => import('pages/calc/Brr.vue') }
+      { path: 'privacy', component: () => import('pages/PrivacyPolicy.vue') }
     ]
   },
 
